72523b9fe3162c1ddd52233665816d8edd35481e
student <student@localhost>
2018-12-18 20:37:46 +0400

    Заполнение классов наследников!

--- src/main/java/ru/itpark/domain/Smartphone.java
+++ src/main/java/ru/itpark/domain/Smartphone.java
@@ -1,4 +1,28 @@
 package ru.itpark.domain;
 
-public class Smartphone {
+public class Smartphone extends Product {
+    private String manufacturer;
+    private String color;
+
+    public Smartphone(int id, String category, String name, int price, String description, String manufacturer, String color) {
+        super(id, category, name, price, description);
+        this.manufacturer = manufacturer;
+        this.color = color;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/main/java/ru/itpark/domain/TShirt.java
+++ src/main/java/ru/itpark/domain/TShirt.java
@@ -1,4 +1,28 @@
 package ru.itpark.domain;
 
-public class TShirt {
+public class TShirt extends Product {
+    private int gender;
+    private int size;
+
+    public TShirt(int id, String category, String name, int price, String description, int gender, int size) {
+        super(id, category, name, price, description);
+        this.gender = gender;
+        this.size = size;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
