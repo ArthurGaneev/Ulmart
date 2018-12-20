e44b92f424e30c3c18664fae5a8ab893884e3930
student <student@localhost>
2018-12-21 01:00:39 +0400

    Заполнение Main

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,7 +1,11 @@
 package ru.itpark;
 
+import ru.itpark.repository.ProductRepository;
+import ru.itpark.service.ProductService;
+
 public class Main {
     public static void main(String[] args) {
+        ProductService service = new ProductService(new ProductRepository());
 
 
     }
